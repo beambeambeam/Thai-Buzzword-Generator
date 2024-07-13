af67f2682cd918f17f25597cd20b1d06df1a2146
beam <supawitmarayat@gmail.com>
2024-07-13 13:00:36 +0700

    feat: change div to input

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.css';
+	import '@fontsource/charm';
 </script>
 
 <slot></slot>
+
+<style>
+	:global(body) {
+		font-family: 'Charm', cursive;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
-	import type { PageData } from './$types';
+	import Input from '$lib/components/ui/input/input.svelte';
 
-	// export let data: PageData;
+	let text = 'สวัสดีครับ ท่านสมาคมคนรักในการใช้ภาษาไทย';
 </script>
 
 <main class="flex h-screen w-screen flex-col items-center justify-center gap-2">
-	<div>Thai Buzzword Generator</div>
-	<div>พูดดีน่ะ แต่ไม่พูดจะดีกว่า</div>
-	<div
-		class="flex h-[5%] w-1/3 items-center justify-center rounded-lg border-2 border-dashed border-black"
-	>
-		test
+	<div class="text-[50px]">Thai Buzzword Generator</div>
+	<div class="text-[25px]">พูดดีน่ะ แต่ไม่พูดจะดีกว่า</div>
+	<div class="flex h-fit w-1/2 flex-row items-center justify-center gap-2">
+		<form class="flex h-full w-full items-center space-x-2">
+			<Input type="email" placeholder="ที่ว่างสำหรับประโยค" disabled />
+			<Button variant="outline" type="submit">คักลอกประโยคนี้</Button>
+		</form>
 	</div>
 	<Button>อุปโลกน์ the word!</Button>
 </main>
