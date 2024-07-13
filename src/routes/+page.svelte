b9c469547e1e068a4fa99319d2a7e7e5c978fb62
beam <supawitmarayat@gmail.com>
2024-07-13 12:43:19 +0700

    feat: basic main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import type { PageData } from './$types';
+
+	// export let data: PageData;
+</script>
+
+<main class="flex h-screen w-screen flex-col items-center justify-center gap-2">
+	<div>Thai Buzzword Generator</div>
+	<div>พูดดีน่ะ แต่ไม่พูดจะดีกว่า</div>
+	<div
+		class="flex h-[5%] w-1/3 items-center justify-center rounded-lg border-2 border-dashed border-black"
+	>
+		test
+	</div>
+	<Button>อุปโลกน์ the word!</Button>
+</main>
